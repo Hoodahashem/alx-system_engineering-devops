8db8227d6d21a2265e0acacbb3131692312e0851
Hoodahashem <mahmoudhashem335@gmail.com>
2024-01-20 01:15:10 +0200

    again

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,17 @@
-# Install a flask package
+# Puppet manifest to install Flask version 2.1.0 on Ubuntu 20.04 LTS
 
-package {'Flask':
+package { 'python3-pip':
+  ensure => installed,
+}
+
+package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3'
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+package { 'werkzeug':
+    ensure   => '2.1.0',
+    provider => 'pip',
+    require  => package['python3-pip'],
 }
