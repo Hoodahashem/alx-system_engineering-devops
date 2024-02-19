36ade5a82c08be7fa4f287e5ee82dcab13a75726
Hoodahashem <mahmoudhashem335@gmail.com>
2024-02-20 01:49:12 +0200

    again

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,33 +1,26 @@
 #!/usr/bin/python3
 """importing requests and sys module"""
 import requests
-import sys
+from sys import argv
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        id = sys.argv[1]
-        request = requests.get("""
-                               https://jsonplaceholder.typicode.com/users/{}\
-""".format(id)).json()
-        employee_name = request.get("name")
-
-        another_one = requests.get("""
-                                   https://jsonplaceholder.typicode.com/users/{}/todos
-""".format(id)).json()
-        false_count = 0
-        true_count = 0
-        total = 0
-        task_title = []
-        for i in another_one:
-            if i["completed"] is False:
-                false_count += 1
-            else:
-                true_count += 1
-                task_title.append(i["title"])
-            total += 1
-
-        print("Employee {} is done with tasks({}/{}):"
-              .format(employee_name, true_count, total))
-        for x in task_title:
-            print("\t {}".format(x))
+    if len(argv) > 1:
+        user = argv[1]
+        url = "https://jsonplaceholder.typicode.com/"
+        req = requests.get("{}users/{}".format(url, user))
+        name = req.json().get("name")
+        if name is not None:
+            json = requests.get(
+                "{}todos?userId={}".format(
+                    url, user)).json()
+            all_tasks = len(json)
+            comp_tasks = []
+            for t in json:
+                if t.get("completed") is True:
+                    comp_tasks.append(t)
+            count = len(comp_tasks)
+            print("Employee {} is done with tasks({}/{}):"
+                  .format(name, count, all_tasks))
+            for title in comp_tasks:
+                print("\t {}".format(title.get("title")))
