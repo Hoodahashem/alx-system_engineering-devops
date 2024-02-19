4933921830e95adc1680849627af0a4b315f12f8
Hoodahashem <mahmoudhashem335@gmail.com>
2024-02-20 01:37:43 +0200

    compleate the requirements

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
+"""importing requests and sys module"""
 import requests
 import sys
 
-if __name__ == '__main__':
 
+if __name__ == "__main__":
     if len(sys.argv) > 1:
         id = sys.argv[1]
-        request = requests.get(f"https://jsonplaceholder.typicode.com/users/{id}").json()
+        request = requests.get("""
+                               https://jsonplaceholder.typicode.com/users/{}
+                               """.format(id)).json()
         employee_name = request.get("name")
 
-        another_one = requests.get(f"https://jsonplaceholder.typicode.com/users/{id}/todos").json()
+        another_one = requests.get("""
+                                   https://jsonplaceholder.typicode.com/users/{}/todos
+                                   """.format(id)).json()
         false_count = 0
         true_count = 0
         total = 0
@@ -22,7 +27,7 @@ if __name__ == '__main__':
                 task_title.append(i["title"])
             total += 1
 
-
-        print("Employee {} is done with tasks({}/{}):".format(employee_name, true_count, total))
+        print("Employee {} is done with tasks({}/{}):"
+              .format(employee_name, true_count, total))
         for x in task_title:
             print("\t {}".format(x))
