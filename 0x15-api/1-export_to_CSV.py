3679c9ebefb1ee80c8d18971f58008c8e4475e16
Hoodahashem <mahmoudhashem335@gmail.com>
2024-02-20 02:12:10 +0200

    2nd task

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""importing requests and sys module"""
+import requests
+import sys
+import csv
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        id = sys.argv[1]
+        request = requests.get("""
+                               https://jsonplaceholder.typicode.com/users/{}\
+""".format(id)).json()
+        employee_name = request.get("username")
+
+        another_one = requests.get("""
+                                   https://jsonplaceholder.typicode.com/users/{}/todos
+""".format(id)).json()
+        false_count = 0
+        true_count = 0
+        total = 0
+        task_title = []
+        for i in another_one:
+            if i["completed"] is False:
+                false_count += 1
+            else:
+                true_count += 1
+                task_title.append(i["title"])
+            total += 1
+
+    with open("{}.csv".format(id), 'w', newline='') as csvfile:
+        taskwriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        for x in another_one:
+            taskwriter.writerow([int(id), employee_name,
+                                x["completed"], x["title"] ])
